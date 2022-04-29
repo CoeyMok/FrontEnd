f80b0699b643ee0cb5a54a2a5607061f99d9c40c
hei yik <hei.yik.mh@perfectmedical.com>
2022-04-29 15:32:45 +0800

    update

---
+++ src/pages/login.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Button from '@mui/material/Button';
+
+function Login() {
+    return (
+        <Button variant="text">Login</Button>
+    )
+}
+
+export default Login;
