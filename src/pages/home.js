8165c585a30cce9cbe3d78b6f63ca8ef5217c0d6
Coey <coeyhoikiu@gmail.com>
2022-05-18 21:58:41 +0800

    Listing page update

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,68 +1,255 @@
 import * as React from 'react';
-import { DataGrid, GridToolbar } from '@material-ui/data-grid';
+import PropTypes from 'prop-types';
 import Button from '@material-ui/core/Button';
 import AddIcon from '@material-ui/icons/Add';
 import IconButton from '@material-ui/core/IconButton';
 import EditIcon from '@material-ui/icons/Edit';
 import DeleteIcon from '@material-ui/icons/DeleteOutlined';
 import SaveIcon from '@material-ui/icons/Save';
+import CancelIcon from '@material-ui/icons/Close';
+import {
+  useGridApiRef,
+  DataGridPro,
+  GridToolbarContainer,
+} from '@mui/x-data-grid-pro';
+import {
+  randomCreatedDate,
+  randomTraderName,
+  randomUpdatedDate,
+  randomId,
+} from '@mui/x-data-grid-generator';
+import { createTheme } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/styles';
 
-const columns = [
-  { field: 'id', headerName: 'ID', width: 90 },
-  {
-    field: 'firstName',
-    headerName: 'First name',
-    width: 150,
-    editable: true,
-  },
-  {
-    field: 'lastName',
-    headerName: 'Last name',
-    width: 150,
-    editable: true,
-  },
-  {
-    field: 'age',
-    headerName: 'Age',
-    type: 'number',
-    width: 110,
-    editable: true,
-  },
-  {
-    field: 'fullName',
-    headerName: 'Full name',
-    description: 'This column has a value getter and is not sortable.',
-    sortable: false,
-    width: 160,
-    valueGetter: (params) =>
-      `${params.getValue(params.id, 'firstName') || ''} ${
-        params.getValue(params.id, 'lastName') || ''
-      }`,
-  },
-];
+const defaultTheme = createTheme();
+
+const useStyles = makeStyles(
+  (theme) => ({
+    root: {
+      display: 'inline-flex',
+      alignItems: 'center',
+      gap: theme.spacing(1),
+      color: theme.palette.text.secondary,
+    },
+    textPrimary: {
+      color: theme.palette.text.primary,
+    },
+  }),
+  { defaultTheme },
+);
+
+function RowMenuCell(props) {
+  const { api, id } = props;
+  const classes = useStyles();
+  const isInEditMode = api.getRowMode(id) === 'edit';
+
+  const handleEditClick = (event) => {
+    event.stopPropagation();
+    api.setRowMode(id, 'edit');
+  };
+
+  const handleSaveClick = (event) => {
+    event.stopPropagation();
+    api.commitRowChange(id);
+    api.setRowMode(id, 'view');
+
+    const row = api.getRow(id);
+    api.updateRows([{ ...row, isNew: false }]);
+  };
+
+  const handleDeleteClick = (event) => {
+    event.stopPropagation();
+    api.updateRows([{ id, _action: 'delete' }]);
+  };
+
+  const handleCancelClick = (event) => {
+    event.stopPropagation();
+    api.setRowMode(id, 'view');
+
+    const row = api.getRow(id);
+    if (row.isNew) {
+      api.updateRows([{ id, _action: 'delete' }]);
+    }
+  };
+
+  if (isInEditMode) {
+    return (
+      <div className={classes.root}>
+        <IconButton
+          color="primary"
+          size="small"
+          aria-label="save"
+          onClick={handleSaveClick}
+        >
+          <SaveIcon fontSize="small" />
+        </IconButton>
+        <IconButton
+          color="inherit"
+          size="small"
+          aria-label="cancel"
+          className={classes.textPrimary}
+          onClick={handleCancelClick}
+        >
+          <CancelIcon fontSize="small" />
+        </IconButton>
+      </div>
+    );
+  }
+
+  return (
+    <div className={classes.root}>
+      <IconButton
+        color="inherit"
+        className={classes.textPrimary}
+        size="small"
+        aria-label="edit"
+        onClick={handleEditClick}
+      >
+        <EditIcon fontSize="small" />
+      </IconButton>
+      <IconButton
+        color="inherit"
+        size="small"
+        aria-label="delete"
+        onClick={handleDeleteClick}
+      >
+        <DeleteIcon fontSize="small" />
+      </IconButton>
+    </div>
+  );
+}
+
+RowMenuCell.propTypes = {
+  api: PropTypes.object.isRequired,
+  id: PropTypes.oneOfType([PropTypes.number, PropTypes.string]).isRequired,
+};
 
 const rows = [
-  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+  {
+    id: randomId(),
+    name: randomTraderName(),
+    age: 25,
+    breed: 'puddle',
+    weight: 27,
+    height: 11
+  },
+  {
+    id: randomId(),
+    name: randomTraderName(),
+    age: 36,
+    breed: 11,
+    weight: 36,
+    height: 11
+  },
+  {
+    id: randomId(),
+    name: randomTraderName(),
+    age: 19,
+    breed: 11,
+    weight: 36,
+    height: 11
+  },
+  {
+    id: randomId(),
+    name: randomTraderName(),
+    age: 28,
+    breed: 2,
+    weight: 36,
+    height: 11
+  },
+  {
+    id: randomId(),
+    name: randomTraderName(),
+    age: 23,
+    breed: 1,
+    weight: 36,
+    height: 11
+  },
 ];
 
+const columns = [
+  { field: 'name', headerName: 'Name', width: 180, editable: true },
+  { field: 'age', headerName: 'Age', type: 'number', editable: true },
+  {
+    field: 'breed',
+    headerName: 'Breed',
+    type: 'number', 
+    editable: true 
+  },
+  { field: 'weight', headerName: 'Weight', width: 180, editable: true },
+  { field: 'height', headerName: 'Height', width: 180, editable: true },
+  {
+    field: 'actions',
+    headerName: 'Actions',
+    renderCell: RowMenuCell,
+    sortable: false,
+    width: 100,
+    headerAlign: 'center',
+    filterable: false,
+    align: 'center',
+    disableColumnMenu: true,
+    disableReorder: true,
+  },
+];
 
-export default function DataTable() {
+function EditToolbar(props) {
+  const { apiRef } = props;
+
+  const handleClick = () => {
+    const id = randomId();
+    apiRef.current.updateRows([{ id, isNew: true }]);
+    apiRef.current.setRowMode(id, 'edit');
+    // Wait for the grid to render with the new row
+    setTimeout(() => {
+      apiRef.current.scrollToIndexes({
+        rowIndex: apiRef.current.getRowsCount() - 1,
+      });
+
+      apiRef.current.setCellFocus(id, 'name');
+    }, 150);
+  };
+
   return (
-    <div style={{ height: 400, width: '100%' }}>
-      <DataGrid
+    <GridToolbarContainer>
+      <Button color="primary" startIcon={<AddIcon />} onClick={handleClick}>
+        Add New Record
+      </Button>
+    </GridToolbarContainer>
+  );
+}
+
+EditToolbar.propTypes = {
+  apiRef: PropTypes.shape({
+    current: PropTypes.object.isRequired,
+  }).isRequired,
+};
+
+export default function FullFeaturedCrudGrid() {
+  const apiRef = useGridApiRef();
+
+  const handleRowEditStart = (params, event) => {
+    event.defaultMuiPrevented = true;
+  };
+
+  const handleRowEditStop = (params, event) => {
+    event.defaultMuiPrevented = true;
+  };
+
+  return (
+    <div style={{ height: 500, width: '100%' }}>
+      <DataGridPro
         rows={rows}
         columns={columns}
-        pageSize={5}
-        checkboxSelection
-        disableSelectionOnClick
+        apiRef={apiRef}
+        editMode="row"
+        onRowEditStart={handleRowEditStart}
+        onRowEditStop={handleRowEditStop}
+        components={{
+          Toolbar: EditToolbar,
+        }}
+        componentsProps={{
+          toolbar: { apiRef },
+        }}
       />
     </div>
   );
