b41ec59bf88085fdf04dc8c536ba2ddf1f562fe0
Coey <coeyhoikiu@gmail.com>
2022-05-23 22:08:22 +0800

    formSuccess

--- src/index.js
+++ src/index.js
@@ -6,23 +6,17 @@ import reportWebVitals from './reportWebVitals';
 import App from './App';
 import Login from './pages/login';
 import Homepage from './pages/homepage'
-<<<<<<< HEAD
 import SignupForm from './pages/SignupForm'
-=======
-import Add from './pages/add'
-
->>>>>>> 9c478da758c2129bb20b4bc7ecbcc353a017caf9
+import FormSuccess from './pages/FormSuccess'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
     <Routes>
          <Route path="/" element={<Homepage/>} />
          <Route path="/login" element={<Login/>}/>
-<<<<<<< HEAD
-         <Route path="/signup" element={<SignupForm />}/>
-=======
          <Route path="/add" element={<Add/>}/>
->>>>>>> 9c478da758c2129bb20b4bc7ecbcc353a017caf9
+         <Route path="/signup" element={<SignupForm/>}/>
+         <Route path="/formSuccess" element={<FormSuccess/>}/>
     </Routes>
   </BrowserRouter>
 );
